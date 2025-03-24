9fab6d529d2313fb9f4c170ad520854fb27f7996
amaan <amaanamn585@gmail.com>
2025-03-25 01:53:44 +0530

    changed index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import jwt from "jsonwebtoken";
 
 import authenticateToken from "./middleware/auth.js";
-import SampleDashboardData from "./carddata.json" assert { type: "json" };
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,7 +14,9 @@ const users = [
   { id: 2, username: "Abrar", password: "Sample2" },
 ];
 
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
+const JWT_SECRET =
+  process.env.JWT_SECRET ||
+  "f25a9d62a6bff5d5e2e8e4daebfa86327d1b214f736dc2c5c38a863ff7ef7e0b";
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -44,6 +45,64 @@ app.post("/api/login", async (req, res) => {
 
 //Dashboard API functionality
 app.get("/api/dashboard", authenticateToken, (req, res) => {
+  const SampleDashboardData = {
+    cards: [
+      {
+        id: 1,
+        title: "Total Users",
+        value: 1200,
+        icon: "users",
+        trend: "up",
+        trendValue: 15,
+        description: "Increase in users compared to last month",
+      },
+      {
+        id: 2,
+        title: "Revenue",
+        value: "$45,000",
+        icon: "dollar",
+        trend: "up",
+        trendValue: 10,
+        description: "Increase in revenue compared to last month",
+      },
+      {
+        id: 3,
+        title: "Active Projects",
+        value: 45,
+        icon: "projects",
+        trend: "down",
+        trendValue: 5,
+        description: "Decrease in active projects compared to last month",
+      },
+      {
+        id: 4,
+        title: "Customer Satisfaction",
+        value: "92%",
+        icon: "smile",
+        trend: "up",
+        trendValue: 3,
+        description: "Increase in customer satisfaction compared to last month",
+      },
+      {
+        id: 5,
+        title: "Support Tickets",
+        value: 120,
+        icon: "ticket",
+        trend: "down",
+        trendValue: 8,
+        description: "Decrease in support tickets compared to last month",
+      },
+      {
+        id: 6,
+        title: "Website Visits",
+        value: "12,000",
+        icon: "globe",
+        trend: "up",
+        trendValue: 20,
+        description: "Increase in website visits compared to last month",
+      },
+    ],
+  };
   res.send(SampleDashboardData);
 });
 
@@ -62,6 +121,6 @@ app.get("/api/map", authenticateToken, (req, res) => {
   res.json(defaultLocation);
 });
 
-app.listen(port, (req, res) => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
